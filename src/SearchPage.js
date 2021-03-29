9177c3997844bac883a445f1c176bd3a97f3fadf
khan Tanveer <kt224945@gmail.com>
2021-03-29 23:35:16 +0530

    commit

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -19,6 +19,46 @@ const SearchPage = (props) => {
 
   const [data, setData] = useState([]);
 
+  //paginate
+
+  const [pageNumber, setPageNumber] = useState(0);
+
+  const userPerPage = 3;
+  const pagesVisited = pageNumber * userPerPage;
+  //
+  const displayUsers = data
+    .slice(pagesVisited, pagesVisited + userPerPage)
+    .map((dat) => {
+      return (
+        <>
+          <div className="search__cardContainer">
+            <div className="search__card">
+              <div className="search__image">
+                <img
+                  className="search__img"
+                  src={dat.links?.[0].href}
+                  // src="https://images-assets.nasa.gov/image/PIA06577/PIA06577~thumb.jpg"
+                  // src="https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100"
+                  alt=""
+                  // alt={dat[0]?.links?.render}
+                />
+              </div>
+              <div className="search__text">
+                <h3>{dat?.data[0]?.title}</h3>
+                <h5>{dat?.data[0]?.date_created}</h5>
+              </div>
+            </div>
+          </div>
+        </>
+      );
+    });
+
+  const pageCount = Math.ceil(data.length / userPerPage);
+
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
   useEffect(() => {
     let datas = localStorage.getItem("myData");
     let newData = JSON.parse(datas);
@@ -34,8 +74,8 @@ const SearchPage = (props) => {
       </button>
       <h1 style={{ textAlign: "center" }}>NASA Media Search</h1>
       <h4 style={{ marginLeft: "1rem" }}>Search Results for</h4>
-      {data.map((dat) => {
-        // console.log("data", dat.links[1].href);
+      {/* {data.map((dat) => {
+        console.log(dat.links?.[0].href);
         return (
           <>
             <div className="search__cardContainer">
@@ -43,8 +83,8 @@ const SearchPage = (props) => {
                 <div className="search__image">
                   <img
                     className="search__img"
-                    // src={dat[0]?.links[0]?.href}
-                    src="https://images-assets.nasa.gov/image/PIA06577/PIA06577~thumb.jpg"
+                    src={dat?.links?.[0].href}
+                    // src="https://images-assets.nasa.gov/image/PIA06577/PIA06577~thumb.jpg"
                     // src="https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100"
                     alt=""
                     // alt={dat[0]?.links?.render}
@@ -58,7 +98,27 @@ const SearchPage = (props) => {
             </div>
           </>
         );
-      })}
+      })} */}
+
+      {displayUsers}
+
+      <ReactPaginate
+        previousLabel={"Previous"}
+        nextLabel={"Next"}
+        pageCount={pageCount}
+        onPageChange={changePage}
+        containerClassName={"paginationBttns"}
+        previousLinkClassName={"previousBttn"}
+        nextLinkClassName={"nextBttn"}
+        disabledClassName={"paginationDisabled"}
+        activeClassName={"paginationActive"}
+      />
+      {/* <br />
+      <br />
+      <br /> */}
+
+      <h1>Related searches</h1>
+
       {/* <div className={classes.root}>
         <Pagination count={10} color="primary" />
       </div> */}
