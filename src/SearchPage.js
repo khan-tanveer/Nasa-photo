5abdea6190bbbbe352f09c331cfdec5c85369ce1
khan Tanveer <kt224945@gmail.com>
2021-03-29 22:11:08 +0530

    commit

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,20 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { useHistory } from "react-router";
 import "./SearchPage.css";
-import { makeStyles } from "@material-ui/core/styles";
-import Pagination from "@material-ui/lab/Pagination";
+import ReactPaginate from "react-paginate";
+// import { makeStyles } from "@material-ui/core/styles";
+// import Pagination from "@material-ui/lab/Pagination";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    "& > *": {
-      marginTop: theme.spacing(2),
-    },
-  },
-}));
+// const useStyles = makeStyles((theme) => ({
+//   root: {
+//     "& > *": {
+//       marginTop: theme.spacing(2),
+//     },
+//   },
+// }));
 
 const SearchPage = (props) => {
   let history = useHistory();
-  const classes = useStyles();
+  // const classes = useStyles();
 
   const [data, setData] = useState([]);
 
@@ -34,7 +35,7 @@ const SearchPage = (props) => {
       <h1 style={{ textAlign: "center" }}>NASA Media Search</h1>
       <h4 style={{ marginLeft: "1rem" }}>Search Results for</h4>
       {data.map((dat) => {
-        console.log("data", dat.links);
+        // console.log("data", dat.links[1].href);
         return (
           <>
             <div className="search__cardContainer">
@@ -42,25 +43,25 @@ const SearchPage = (props) => {
                 <div className="search__image">
                   <img
                     className="search__img"
-                    // src={dat[0]?.links[0].href}
+                    // src={dat[0]?.links[0]?.href}
                     src="https://images-assets.nasa.gov/image/PIA06577/PIA06577~thumb.jpg"
                     // src="https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100"
-                    // alt=""
-                    alt={dat[0]?.links[0].render}
+                    alt=""
+                    // alt={dat[0]?.links?.render}
                   />
                 </div>
                 <div className="search__text">
-                  <h3>{dat?.data[0].title}</h3>
-                  <h5>{dat?.data[0].date_created}</h5>
+                  <h3>{dat?.data[0]?.title}</h3>
+                  <h5>{dat?.data[0]?.date_created}</h5>
                 </div>
               </div>
             </div>
           </>
         );
       })}
-      <div className={classes.root}>
+      {/* <div className={classes.root}>
         <Pagination count={10} color="primary" />
-      </div>
+      </div> */}
     </div>
   );
 };
