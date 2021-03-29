9e6e9989d56ba4896fb53e054bdb94634072103e
khan Tanveer <kt224945@gmail.com>
2021-03-29 20:05:52 +0530

    commit

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -34,15 +34,19 @@ const SearchPage = (props) => {
       <h1 style={{ textAlign: "center" }}>NASA Media Search</h1>
       <h4 style={{ marginLeft: "1rem" }}>Search Results for</h4>
       {data.map((dat) => {
+        console.log("data", dat.links);
         return (
           <>
             <div className="search__cardContainer">
               <div className="search__card">
                 <div className="search__image">
                   <img
-                    src={dat?.data[0].href}
+                    className="search__img"
+                    // src={dat[0]?.links[0].href}
+                    src="https://images-assets.nasa.gov/image/PIA06577/PIA06577~thumb.jpg"
                     // src="https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100"
-                    alt={dat?.data[0].media_type}
+                    // alt=""
+                    alt={dat[0]?.links[0].render}
                   />
                 </div>
                 <div className="search__text">
